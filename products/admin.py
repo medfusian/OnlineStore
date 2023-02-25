5a520d7862d2dd53e70c31bcd07ebfdf0248433a
medfusian <vlad.shumihai@gmail.com>
2023-02-25 19:14:44 +0300

    Working with filtering, pagination, Django Admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity',)
+    extra = 0
--- products/models.py
+++ products/models.py
@@ -12,6 +12,10 @@ class ProductCategory(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -23,6 +27,11 @@ class Product(models.Model):
 
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
 
 
 class BasketQuerySet(models.QuerySet):
